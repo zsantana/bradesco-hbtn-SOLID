86dd9e516cde68c8908699877a7e0e34dec803e4
zsantana <reinaldojsantana@gmail.com>
2025-10-02 14:09:54 +0000

    Principios SOLID

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -3,9 +3,9 @@ public class Comments {
     // Copyright (C) 2022 by CleanCode.
     // Lançado sob os termos da GNU General Public License versão 2 ou posterior
 
-    public Owner getTestcaseOwner(Testcase testcase) { } 
+    public Owner getTestcaseOwner(Testcase testcase) { }
 
-    public Hours getHoursInQuarter() { } 
+    public Hours getHoursInQuarter() { }
 
     // TODO Este método deve ser removido
     public void check() {
@@ -13,10 +13,11 @@ public class Comments {
     }
 
     /**
-     * <p>Este método publica resultados com base na data.</p>
-     * @param date data a partir da qual os resultados devem ser publicados
-     * @return Lista de entidades de resultado
-     * @since 16.0
+     * <p> este método publica resultados com base na data
+     * </p>
+     * @param data data a partir da qual os resultados devem ser publicados
+     * @retornar Lista de entidades de resultado
+     * @desde 16.0
      */
     public List<Result> publishResults(Date date) {
         // alguma lógica de negócios aqui
